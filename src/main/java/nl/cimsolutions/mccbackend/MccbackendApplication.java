0d13e8cc9c6292a6b9b33b3dc970bd53d31fd52d
Marinkton <marinkton@hotmail.com>
2019-10-07 15:26:11 +0200

    "added owner"

--- src/main/java/nl/cimsolutions/mccbackend/MccbackendApplication.java
+++ src/main/java/nl/cimsolutions/mccbackend/MccbackendApplication.java
@@ -1,44 +1,42 @@
 package nl.cimsolutions.mccbackend;
 
-import nl.cimsolutions.mccbackend.datasource.KnmiCollector;
-import nl.cimsolutions.mccbackend.model.Location;
-import nl.cimsolutions.mccbackend.model.Research;
-import nl.cimsolutions.mccbackend.model.Temperature;
-import nl.cimsolutions.mccbackend.model.Voyager;
-import nl.cimsolutions.mccbackend.model.types.VoyagerSensors;
-import nl.cimsolutions.mccbackend.repository.LocationRepository;
-import nl.cimsolutions.mccbackend.repository.ResearchRepository;
-import nl.cimsolutions.mccbackend.repository.VoyagerRepository;
-import nl.cimsolutions.mccbackend.util.DummyDataGenerator;
+
+import nl.cimsolutions.mccbackend.model.*;
+import nl.cimsolutions.mccbackend.repository.DataSourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.*;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class MccbackendApplication implements CommandLineRunner {
 
     @Autowired
-    VoyagerRepository voyagerRepository;
-
-    @Autowired
-    LocationRepository locationRepository;
-
-    @Autowired
-    ResearchRepository researchRepository;
+    DataSourceRepository dataSourceRepository;
 
 	public static void main(String[] args) throws IOException, ParseException {
 		SpringApplication.run(MccbackendApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception { }
+	public void run(String... args) throws Exception {
+//        DataSource knmi = new DataSource("KNMI", "www.knmi.nl");
+//        DataSource luchtmeetnet = new DataSource("Luchtmeetnet", "www.luchtmeetnet.nl");
+//
+//        WeatherStation weatherStation1 = new WeatherStation("Amsterdam", "1234", 1.23, 3.456);
+//        WeatherStation weatherStation2 = new WeatherStation("Groningen", "5678", 1.23, 3.456);
+//        knmi.getWeatherStations().add(weatherStation1);
+//        luchtmeetnet.getWeatherStations().add(weatherStation2);
+//        dataSourceRepository.save(knmi);
+//        dataSourceRepository.save(luchtmeetnet);
+//
+    }
 
 
 
